df88fdaf6cfa5d0b8fa17242382fb52ae1bb4b4d
SLWhite <stevenw.bama88@gmail.com>
2020-06-06 12:35:03 -0400

    numeric formatting

--- PyBank/mainbanksolved.py
+++ PyBank/mainbanksolved.py
@@ -60,9 +60,22 @@ with open(budget_csv, 'r') as csvfile:
     print ("Greatest Monthly Increase in Profit: $", format (poschange, ",.0f"),"on", posdate)
     print ("Greatest Monthly Decrease in Profit: $", format (negchange, ",.0f"),"on", negdate)
     
+#output txt file
+output_file = os.path.join("pybank_financial_analysis.txt")  #confirmed syntax from https://stackoverflow.com/questions/6159900/correct-way-to-write-line-to-file
+with open(output_file, "w") as file:
+    
+    file.write("Financial Analysis\n")
+    file.write("--------------------\n")            
+    file.write(f'Total Months: {months}\n')
+    file.write(f'Total P&L: $ {pltotal:,}\n')
+    file.write(f'Average Change: $ {avgchange:,.2f}\n')
+    file.write(f'Greatest Monthly Increase in Profit: $ {poschange:,} on {posdate}\n')
+    file.write(f'Greatest Monthly Decrease in Profit: $ {negchange:,} on {negdate}\n')
 
 
 
+    
+    
 
         
 
